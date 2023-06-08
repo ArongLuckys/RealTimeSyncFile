b9f04e62437c2f351d666798e13f008beaedc81b
Forgotten <2786217208@qq.com>
2023-06-08 15:57:43 +0800

    添加项目文件。

---
+++ RealTimeSyncFile/Main.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace RealTimeSyncFile
+{
+	partial class Main
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.tabControl2 = new System.Windows.Forms.TabControl();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.listBox3 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabControl2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.Padding = new System.Drawing.Point(15, 5);
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(791, 423);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.button5);
+			this.tabPage1.Controls.Add(this.button4);
+			this.tabPage1.Controls.Add(this.button3);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.tabControl2);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 26);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(783, 393);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "消息";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(388, 35);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(150, 23);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "将服务器缺失的文件更新";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(232, 35);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(150, 23);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "将本机缺失的文件更新";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(463, 6);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(130, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "列出服务器缺失文件";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(337, 6);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(120, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "列出本机缺失文件";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// tabControl2
+			// 
+			this.tabControl2.Controls.Add(this.tabPage3);
+			this.tabControl2.Controls.Add(this.tabPage4);
+			this.tabControl2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tabControl2.Location = new System.Drawing.Point(225, 97);
+			this.tabControl2.Name = "tabControl2";
+			this.tabControl2.Padding = new System.Drawing.Point(15, 5);
+			this.tabControl2.SelectedIndex = 0;
+			this.tabControl2.Size = new System.Drawing.Size(555, 293);
+			this.tabControl2.TabIndex = 2;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.listBox2);
+			this.tabPage3.Location = new System.Drawing.Point(4, 26);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(547, 263);
+			this.tabPage3.TabIndex = 0;
+			this.tabPage3.Text = "本机缺失的文件";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// listBox2
+			// 
+			this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 12;
+			this.listBox2.Location = new System.Drawing.Point(3, 3);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(541, 257);
+			this.listBox2.TabIndex = 0;
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.listBox3);
+			this.tabPage4.Location = new System.Drawing.Point(4, 26);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage4.Size = new System.Drawing.Size(516, 263);
+			this.tabPage4.TabIndex = 1;
+			this.tabPage4.Text = "服务器缺失的文件";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// listBox3
+			// 
+			this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox3.FormattingEnabled = true;
+			this.listBox3.ItemHeight = 12;
+			this.listBox3.Location = new System.Drawing.Point(3, 3);
+			this.listBox3.Name = "listBox3";
+			this.listBox3.Size = new System.Drawing.Size(510, 257);
+			this.listBox3.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(231, 6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "获取网盘文件";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(3, 3);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(222, 387);
+			this.listBox1.TabIndex = 0;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.textBox2);
+			this.tabPage2.Controls.Add(this.label2);
+			this.tabPage2.Controls.Add(this.textBox1);
+			this.tabPage2.Controls.Add(this.label1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 26);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(752, 420);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "设置";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Location = new System.Drawing.Point(10, 77);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(734, 21);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(77, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "本地文件路径";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(10, 29);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(734, 21);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "同步网盘路径";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(791, 423);
+			this.Controls.Add(this.tabControl1);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "实时同步文件";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabControl2.ResumeLayout(false);
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage4.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TabControl tabControl2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.ListBox listBox3;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ RealTimeSyncFile/Main.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RealTimeSyncFile
+{
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		List<string> files = new List<string>();
+		//临时文件
+		string infopath = Directory.GetCurrentDirectory() + "\\RealTimeSynFileTemp.txt";
+
+		/// <summary>
+		/// 计时器 分析当前文件时间
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			listBox1.SelectedIndex = listBox1.Items.Count - 1;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.fileway = textBox1.Text;
+			Properties.Settings.Default.Save();
+		}
+
+		/// <summary>
+		///  窗体启动实例
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			textBox1.Text = Properties.Settings.Default.fileway;
+
+
+			if (!File.Exists(infopath))
+			{
+				File.AppendAllText(infopath, "");
+			}
+			else
+			{
+				File.Delete(Directory.GetCurrentDirectory() + "\\RealTimeSynFileTemp.txt");
+			}
+		}
+
+		/// <summary>
+		/// 获取网盘文件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			files.Clear();
+			listBox1.Items.Add("开始获取文件");
+			string info = "";
+			string path = textBox1.Text;
+			//获取网盘的所有路径，并且将信息写入到日志内
+			DateTime KS = DateTime.Now;
+			await Task.Run(() =>
+			{
+				files.AddRange(Directory.GetFiles(textBox1.Text, "*", SearchOption.AllDirectories));
+				for (int i = 0; i < files.Count; i++)
+				{
+					info = File.GetLastWriteTime(files[i]).ToString();
+					info += "=" + files[i].ToString().Replace(path, "") + "\n";
+					File.AppendAllText(infopath, info);
+				}
+			});
+			DateTime JS = DateTime.Now;
+			TimeSpan T = JS - KS;
+			listBox1.Items.Add("文件目录获取成功,耗时 " + T);
+			listBox1.Items.Add("文件数量 " + files.Count);
+		}
+	}
+}
